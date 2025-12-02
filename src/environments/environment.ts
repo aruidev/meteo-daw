77c2098d08e98ed8bcd48fdb3ea1d989e622de69
Alex Ruiz <a.ruiz7@sapalomera.cat>
2025-12-02 15:24:04 +0100

    Configure production environment settings and update file references

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production : false,
+    weatherApiBaseUrl : '',
+    options: {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': '',
+            'x-rapidapi-host': ''
+        }
+    },
+}
